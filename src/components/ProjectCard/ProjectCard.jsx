575b4f954d8ebe80bd1a6f98a6a14bab9a6b17ca
harzarul <harzarul@yahoo.com>
2022-12-06 12:35:39 +0800

    installed gh-pages

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -2,7 +2,7 @@
 import {motion} from 'framer-motion';
 import styles from '../../styles';
 
-const ProjectCard = ({id, imgUrl, title, link, index, active, handleClick}) => {
+const ProjectCard = ({id, imgUrl, title, link, index, ps, active, handleClick}) => {
     return (
     <div className={`${active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'} flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer relative`}
         onClick={() => handleClick(id)}>
@@ -17,7 +17,7 @@ const ProjectCard = ({id, imgUrl, title, link, index, active, handleClick}) => {
         ) : (
             <div className="absolute bottom-0 p-8 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
                 <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase">
-                    Enter Metaverse
+                    {ps}
                 </p>
                 <a href={link} target='_blank'>
                     <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white hover:text-gold cursor-pointer">
